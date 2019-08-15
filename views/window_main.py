3c615eae036855ed010c52cac589f4e3b309e497
Bill Guo <billguo.feather@outlook.com>
2019-08-15 15:33:49 +0800

    重构window_main.py，将子部件的构建分离成独立函数

--- views/window_main.py
+++ views/window_main.py
@@ -9,28 +9,25 @@ class WindowMain(QtWidgets.QWidget):
         super().__init__()
 
         # 设置窗口大小
-        window_width = screen_width * 0.2
-        window_height = screen_height * 0.8
-        self.resize(window_width, window_height)
+        self.window_width = screen_width * 0.2
+        self.window_height = screen_height * 0.8
+        self.resize(self.window_width, self.window_height)
 
-        self.setWindowOpacity(0.5)  # 设置窗口透明度
+        # 设置窗口透明度
+        self.setWindowOpacity(0.5)
+        # 设置窗口无边框
         self.setWindowFlags(
             QtCore.Qt.FramelessWindowHint
-        )  # 设置窗口无边框
+        )
+        # 设置窗口背景透明
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
-        self.widget_head = QtWidgets.QWidget()
-        self.widget_head.setObjectName('head')
-        self.widget_head.setFixedSize(window_width, window_height * 0.2)
+        # 设置窗口子部件
+        self.set_head_widget()
+        self.set_body_widget()
+        self.set_bottom_widget()
 
-        self.widget_body = QtWidgets.QWidget()
-        self.widget_body.setObjectName('body')
-        self.widget_body.setFixedSize(window_width, window_height * 0.74)
-
-        self.widget_bottom = QtWidgets.QWidget()
-        self.widget_bottom.setObjectName('bottom')
-        self.widget_bottom.setFixedSize(window_width, window_height * 0.06)
-
+        # 窗口组装
         self.layout_main = QtWidgets.QVBoxLayout()
         self.layout_main.setSpacing(0)
         self.layout_main.addWidget(self.widget_head)
@@ -38,4 +35,29 @@ class WindowMain(QtWidgets.QWidget):
         self.layout_main.addWidget(self.widget_bottom)
         self.setLayout(self.layout_main)
 
+        # 设置样式
         QSSSetter.set_qss(self, __file__)
+
+    def set_head_widget(self):
+        self.widget_head = QtWidgets.QWidget()
+        self.widget_head.setObjectName('head')
+        self.widget_head.setFixedSize(
+            self.window_width,
+            self.window_height * 0.2
+        )
+
+    def set_body_widget(self):
+        self.widget_body = QtWidgets.QWidget()
+        self.widget_body.setObjectName('body')
+        self.widget_body.setFixedSize(
+            self.window_width,
+            self.window_height * 0.74
+        )
+
+    def set_bottom_widget(self):
+        self.widget_bottom = QtWidgets.QWidget()
+        self.widget_bottom.setObjectName('bottom')
+        self.widget_bottom.setFixedSize(
+            self.window_width,
+            self.window_height * 0.06
+        )
