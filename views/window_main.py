abe7081272b2fea0117f7f9c7636bfa0f141f60b
Bill Guo <billguo.feather@outlook.com>
2019-08-16 14:23:35 +0800

    完成用户头像、用户信息、环境选择下拉框和商户搜索组件的创建及布局

--- views/window_main.py
+++ views/window_main.py
@@ -11,7 +11,8 @@ class WindowMain(QtWidgets.QWidget):
         # 设置窗口大小
         self.window_width = screen_width * 0.2
         self.window_height = screen_height * 0.8
-        self.resize(self.window_width, self.window_height)
+        self.setMinimumSize(400, 760)
+        self.setMaximumSize(400, 960)
 
         # 设置窗口透明度
         self.setWindowOpacity(0.7)
@@ -30,11 +31,11 @@ class WindowMain(QtWidgets.QWidget):
 
         # 窗口组装
         self.layout_main = QtWidgets.QVBoxLayout()
-        self.layout_main.setSpacing(0)
         self.layout_main.addWidget(self.widget_window_buttons)
         self.layout_main.addWidget(self.widget_head)
         self.layout_main.addWidget(self.widget_body)
         self.layout_main.addWidget(self.widget_bottom)
+        self.layout_main.setSpacing(0)
         self.setLayout(self.layout_main)
 
         # 设置样式
@@ -50,9 +51,11 @@ class WindowMain(QtWidgets.QWidget):
         layout.setSpacing(0)
 
         button_min_window = QtWidgets.QPushButton()
+        button_min_window.clicked.connect(self.showMinimized)
         button_min_window.setObjectName('button_min_window')
         button_min_window.setFixedSize(25, 25)
         button_close_window = QtWidgets.QPushButton()
+        button_close_window.clicked.connect(QtWidgets.qApp.quit)
         button_close_window.setFixedSize(25, 25)
         button_close_window.setObjectName('button_close_window')
 
@@ -60,28 +63,76 @@ class WindowMain(QtWidgets.QWidget):
         layout.addWidget(button_min_window)
         layout.addStretch(1)
         layout.addWidget(button_close_window)
+        layout.addStretch(1)
         self.widget_window_buttons.setLayout(layout)
 
     def set_head_widget(self):
         self.widget_head = QtWidgets.QWidget()
         self.widget_head.setObjectName('head')
-        self.widget_head.setFixedSize(
-            self.window_width,
-            self.window_height * 0.2 - 40
-        )
+        self.widget_head.setFixedSize(384, 120)
+
+        # 用户头像和用户信息组件
+        label_photo = QtWidgets.QLabel()
+        label_photo.setFixedSize(80, 80)
+        label_photo.setObjectName('user_photo')
+        label_username = QtWidgets.QLabel('Bill Guo')
+        label_username.setObjectName('user_name')
+        label_userinfo = QtWidgets.QLabel('Administrator')
+        label_userinfo.setObjectName('user_info')
+
+        # 布局
+        layout = QtWidgets.QHBoxLayout()
+        layout.setSpacing(0)
+        layout.addWidget(label_photo)
+        layout_v = QtWidgets.QVBoxLayout()
+        layout_v.setSpacing(0)
+        layout.addLayout(layout_v)
+        layout_v.addWidget(label_username)
+        layout_v.addWidget(label_userinfo)
+        self.widget_head.setLayout(layout)
 
     def set_body_widget(self):
         self.widget_body = QtWidgets.QWidget()
         self.widget_body.setObjectName('body')
-        self.widget_body.setFixedSize(
-            self.window_width,
-            self.window_height * 0.74
-        )
+        self.widget_body.setMinimumSize(384, 550)
+        self.widget_body.setMaximumSize(384, self.window_height - 40 - 120)
+
+        # 环境选择下拉框和商户搜索组件
+        combobox_env = QtWidgets.QComboBox()
+        combobox_env.setObjectName('env')
+        combobox_env.setFixedSize(384, 30)
+        label_search = QtWidgets.QLabel()
+        label_search.setObjectName('search_icon')
+        label_search.setFixedSize(30, 30)
+        lineedit_search = QtWidgets.QLineEdit()
+        lineedit_search.setObjectName('search_tenant')
+        lineedit_search.setFixedSize(354, 30)
+
+        # 布局
+        layout = QtWidgets.QVBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout_h = QtWidgets.QHBoxLayout()
+        layout_h.setSpacing(0)
+        layout_h.addWidget(label_search)
+        layout_h.addWidget(lineedit_search)
+        layout.addWidget(combobox_env)
+        layout.addLayout(layout_h)
+        layout.addStretch()
+        self.widget_body.setLayout(layout)
 
     def set_bottom_widget(self):
         self.widget_bottom = QtWidgets.QWidget()
         self.widget_bottom.setObjectName('bottom')
-        self.widget_bottom.setFixedSize(
-            self.window_width,
-            self.window_height * 0.06
-        )
+        self.widget_bottom.setFixedSize(384, 40)
+
+    def mousePressEvent(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.dragPos = event.globalPos()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
